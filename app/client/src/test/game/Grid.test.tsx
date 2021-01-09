e37c504a4476d8d8d01ff659fd6ee971df4c67a0
Valery Teplyakov <Valeriy.Teplyakov@jetbrains.com>
2021-01-09 06:40:03 +0300

    Fix grid tests

--- app/client/src/test/game/Grid.test.tsx
+++ app/client/src/test/game/Grid.test.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import {mount} from 'enzyme'
+import {mount, ReactWrapper} from 'enzyme'
+import Game from "../../app/game/Game";
+import {UnknownUser} from "../../app/utils/CurrentUser";
 import Grid from "../../app/game/Grid";
+
+function mountGrid(columns: number, rows: number, seed: number): ReactWrapper<Grid["props"], Grid["state"], Grid> {
+  const game = mount<Game, Game["props"], Game["state"]>(<Game currentUser={new UnknownUser()}/>)
+  game.instance().onSettingsChange(columns, rows, seed)
+  game.update()
+  return game.children().childAt(0) as ReactWrapper<Grid["props"], Grid["state"], Grid>
+}
 
 describe("Grid", () => {
 
   it("Grid size", () => {
     const cases = [[40, 20], [60, 15], [13, 15]]
     cases.forEach(testCase => {
-      const grid = mount(<Grid columns={testCase[0]} rows={testCase[1]} seed={0}/>)
+      const grid = mountGrid(testCase[0], testCase[1], 0)
       expect(grid.find(".cell").length).toEqual(testCase[0] * testCase[1])
     });
   })
@@ -15,18 +24,18 @@ describe("Grid", () => {
   it("Grid 10x10 seed 0", () => {
     const expectedGrid =
       [
-        true, false, false, true, false, false, false, false, true, false,
-        false, false, false, true, true, false, false, true, false, false,
-        false, false, false, false, true, false, false, false, false, true,
-        false, false, false, false, false, false, false, true, false, false,
-        false, true, true, true, true, false, false, false, true, true,
-        false, false, false, false, true, true, true, false, false, false,
-        false, false, false, false, false, false, false, true, false, false,
-        true, false, false, false, true, false, false, true, false, false,
-        false, true, true, false, true, true, false, false, false, false,
-        false, false, false, false, true, false, false, false, true, false
+        false, false, false, false, false, false, false, false, false, false,
+        false, false, false, false, true, false, false, false, false, false,
+        false, false, false, false, false, false, false, false, false, false,
+        false, false, false, false, false, false, false, false, false, false,
+        false, false, false, false, false, false, false, false, true, true,
+        false, false, false, false, false, true, false, false, false, false,
+        false, false, false, false, false, false, false, false, false, false,
+        false, false, false, false, false, false, false, false, false, false,
+        false, false, false, false, false, false, false, false, false, false,
+        false, false, false, false, false, false, false, false, false, false
       ]
-    const grid = mount(<Grid columns={10} rows={10} seed={0}/>)
+    const grid = mountGrid(10, 10, 0)
     let gridCells = grid.children().children().map((val) => val.props().isAlive);
     expect(gridCells).toEqual(expectedGrid)
   })
